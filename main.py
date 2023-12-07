ffb5c53d8856dc3398d9576cb9483fc132f486bf
Daniel-Burbridge-Developer <Daniel.John.Developer@gmail.com>
2023-12-07 21:35:28 +0800

    Basic functionality of importing book, and counting words completed.

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-path_to_folder = "books/"
-file_name = "frankenstein.txt"
-path_to_file = path_to_folder + file_name
+def main():
 
-with open(path_to_file) as f:
-    file_contents = f.read()
+    path_to_folder = "books/"
+    file_name = "frankenstein.txt"
+    book = set_book(path_to_folder, file_name)
+    print(get_num_words(book))
+
+def set_book(path_to_folder, file_name):
+    path_to_file = path_to_folder + file_name
+
+    with open(path_to_file) as f:
+        return f.read()
     
+def get_num_words(book):
+    return len(book.split())
+
+if __name__ == "__main__":
+    main()
+
